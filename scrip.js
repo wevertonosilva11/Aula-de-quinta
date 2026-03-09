d580ff80ea253b623ad15cba94a1dd7bd662fa06
wevertonosilva11 <weverton.o.silva11@aluno.senai.br>
2026-03-09 10:50:16 -0500

    Atualizando atv namora cmg

--- scrip.js
+++ scrip.js
@@ -1,6 +1,10 @@
 const btnSim = document.getElementById("btnSim")
 const btnNao = document.getElementById("btnNao")
 
+btnSim.addEventListener("click", aceitar);
+btnNao.addEventListener("mouseover", desviar);
+btnNao.addEventListener("click", desviar);
+
 function aceitar(){
     alert("Você aceitou namorar comigo! :)")
     location.href = "https://youtu.be/lV2plyhSRBU?si=dWMXxho0CDt2S8Rj"
@@ -8,6 +12,10 @@ function aceitar(){
 
 function desviar(){
     btnNao.style.position = "absolute";
+    btnNao.style.bottom = geraPosicao(10, 90);
+    btnNao.style.left = geraPosicao(10, 90);
+
+    console.log("opa, desviei...");
 }
 
 function geraPosicao(min,max){
